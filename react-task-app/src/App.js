c64405a701550176f1b51c410a2c82930c61aacd
Jordan <jordan@chude.co>
2021-11-24 18:11:51 -0500

    updated notes and header imported to  app.js

--- react-task-app/src/App.js
+++ react-task-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
+// JSX, NOT HTML
 function App() {
+  // const name = 'Brad';
+  // const x = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // div name can be semantic
+    // can be empty if you don't want to signify it
+    // can write dynamic javascript
+    <div className="container">
+      {/* <h1>Hello From React</h1>
+      <h1>Hello {name}</h1>
+      <h1>Hello {x ? 'Yes' : 'No'}</h1> */}
+
+      
+      <Header />
     </div>
   );
 }
